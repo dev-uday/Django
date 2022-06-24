0975fa19d628e94328c273b7124e015f81be8f2c
uday-vispan <uday.vispan@gmail.com>
2022-06-24 12:18:04 +0530

    py templats

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 
-# Create your views here.
+def index(request):
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
